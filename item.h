5be2781041502fefc1257018f6d7892b0d50d37a
Olle Mattsson <rymdolle@gmail.com>
2023-11-14 13:40:14 +0100

    Change variable names

--- item.h
+++ item.h
@@ -25,48 +25,46 @@ public:
     std::cout << "Insert coins.\n"
               << "Write coin denomination in kronor e.g. 1,2,5,10 "
               << "or abort with '0'.\n";
-    int change = 0;
-    while (change < cost_) {
-      std::cout << "Left to pay: " << cost_ - change << " kr\n";
-      int payment;
-      std::cin >> payment;
-      switch (payment) {
+    int payment = 0;
+    while (payment < cost_) {
+      std::cout << "Left to pay: " << cost_ - payment << " kr\n";
+      int coin;
+      std::cin >> coin;
+      switch (coin) {
       case 1:
       case 2:
       case 5:
       case 10:
-        change += payment;
+        payment += coin;
         break;
       case 0:
         std::cout << "You have chosen to abort.\n";
         // Give back change and exit program if user aborts
-        get_change(change + cost_);
+        get_change(payment);
         exit(1);
       default:
         std::cout << "Invalid denomination. Try again or abort with '0'.\n";
         break;
       }
     }
-    return change;
+    return payment - cost_;
   }
 
-  void get_change(int change)
+  static void get_change(int change)
   {
-    // Get the absolute value for the change
-    int back = std::abs(cost_ - change);
     std::cout << '\n'
-              << "Change back: " << back << " kr.\n";
+              << "Change back: " << change << " kr.\n";
 
     int coin = 10;
     do {
-      int amount = back / coin;
+      int amount = change / coin;
       if (amount > 0) {
-        back -= amount * coin;
+        change -= amount * coin;
         std::cout << amount << " x " << coin << " kr.\n";
       }
       // Use integer division to get next coin
       coin /= 2;
-    } while (back > 0);
+    } while (change > 0);
   }
 };
 
